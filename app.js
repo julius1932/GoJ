f49e5f6b74b9ffeb3cbc49e36e9961b24865de4e
julius1932 <lasjulius@gmail.com>
2018-11-09 13:24:24 +0200

    update

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const path = require('path');
 const fs = require('fs');
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname');
 const app = express();
+
+const _DB = require('./db');
+
 app.set('port', process.env.PORT || 3000);
 // Middlewares
 // set morgan to log info about our requests for development use.
@@ -24,10 +24,36 @@ app.get("/", function(req, res) {
 app.get("/pp", function(req, res) {
     res.sendFile(__dirname + '/password.html');
 });
-app.post('/login', function(req, res) {
-   var username = req.body.username;
-   var password = req.body.password;
-   res.jsonp({pp:"loged"});
+app.get("/user/:username:password", function(req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    var qry = { username: username, password: password };
+    _DB.findModel("USER", qry, function(results) {
+        console.log(results);
+        //do the loging in
+    });
+});
+app.get("/user", function(req, res) {
+    _DB.findModelAll("USER", function(results) {
+        console.log(results);
+    });
+});
+app.post('user', function(req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    var data={};
+    _DB.createModel("USER", data, function(result) {
+        console.log(result);
+    });
+});
+app.put('user', function(req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    var qry={};
+    var data={};
+    _DB.updateModel(model, qry, data,  function(result) {
+        console.log(result);
+    });
 });
 
 app.post('/s3', function(req, res) {
---
+++ db.js
@@ -0,0 +1,126 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('mydb', 'postgres', 'root', {
+    host: 'localhost',
+    dialect: 'postgres',
+    operatorsAliases: false,
+    pool: { max: 5, min: 0, acquire: 30000, idle: 10000 },
+});
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+
+const USER = sequelize.define('user', {
+    firstName: { type: Sequelize.STRING },
+    lastName: { type: Sequelize.STRING }
+});
+const KLASS = sequelize.define('klass', {
+    id: { type: Sequelize.UUID, allowNull: false, primaryKey: true },
+    level: { type: Sequelize.INTEGER, allowNull: false, unique: 'compositeIndex' },
+    division: { type: Sequelize.TEXT, allowNull: false, unique: 'compositeIndex' }
+});
+const LEARNER = sequelize.define('learner', {
+    id: { type: Sequelize.UUID, allowNull: false, primaryKey: true },
+    fname: { allowNull: false, type: Sequelize.STRING },
+    sname: { allowNull: false, type: Sequelize.STRING },
+    gender: { allowNull: false, type: Sequelize.STRING },
+    email: { type: Sequelize.TEXT, allowNull: false, validate: { isEmail: true } },
+    phone: { type: Sequelize.STRING, allowNull: false }
+});
+const FEESSETUP = sequelize.define('feesSetUp', {
+    id: { type: Sequelize.UUID, allowNull: false, primaryKey: true },
+    amount: { type: Sequelize.BIGINT(11), allowNull: false },
+    purpose: { type: Sequelize.STRING, allowNull: false },
+    mandetory: { type: Sequelize.BOOLEAN, allowNull: false },
+    type: { type: Sequelize.STRING, allowNull: false }, //fixed
+    crateria: { type: Sequelize.STRING, allowNull: false }, //if subject bassed
+    whoShouldPay: { type: Sequelize.ARRAY(Sequelize.TEXT), allowNull: false },
+    accountType: { type: Sequelize.STRING, allowNull: false },
+});
+const INVOICE = sequelize.define('invoice', {
+    id: { type: Sequelize.UUID, allowNull: false, primaryKey: true },
+    paymentProgress: { type: Sequelize.STRING, allowNull: false }, // nothing, inprogress , done
+});
+const PAYMENT = sequelize.define('payment', {
+    id: { type: Sequelize.UUID, allowNull: false, primaryKey: true },
+    paymentMethod: { type: Sequelize.STRING, allowNull: false }
+});
+
+KLASS.hasMany(LEARNER);
+
+LEARNER.hasMany(INVOICE);
+FEESSETUP.hasMany(INVOICE);
+
+LEARNER.hasMany(PAYMENT);
+INVOICE.hasMany(PAYMENT);
+
+sequelize.sync({ force: false }).then(() => {
+    // Table created
+    //return model.create();
+});
+const MODELS = { USER: USER, KLASS: KLASS, LEARNER: LEARNER, INVOICE: INVOICE, PAYMENT: PAYMENT, FEESSETUP: FEESSETUP }
+
+function checkModel(model) {
+    
+    return MODELS[model];
+}
+const _DB = {
+    sequelize: sequelize,
+    createModel: function(model, qry, callback) {
+        if (checkModel(model)) {
+            MODELS[model].create(qry).then(result => {
+                callback(result);
+            });
+        }
+    },
+    findModelAll: function(model, callback) {
+        if (checkModel(model)) {
+            MODELS[model].findAll().then(results => {
+                callback(results);
+            });
+        }
+    },
+    findModel: function(model, qry, callback) {
+        if (checkModel(model)) {
+            MODELS[model].find(qry).then(results => {
+                callback(results);
+            });
+        }
+    },
+    findModelById: function(model, callback) {
+        if (checkModel(model)) {
+            MODELS[model].findById(customerId).then(result => {
+                callback(result);
+            })
+        }
+    },
+    updateModel: function(model, qry, data, callback) {
+        if (checkModel(model)) {
+            MODELS[model].update(qry, { where: data }).then(() => {
+                callback();
+            });
+        }
+    },
+    deleteModel: function(model, qry, callback) {
+        if (checkModel(model)) {
+            MODELS[model].destroy({
+                where: qry
+            }).then(() => {
+                callback();
+            });
+        }
+    }
+}
+/*_DB.createModel("USER",{firstName:"lastborn",lastName:"Julius"},function(result){
+    console.log(result);
+});*/
+_DB.findModelAll("USER",function(results){
+    console.log(results);
+});
+module.exports = _DB;
